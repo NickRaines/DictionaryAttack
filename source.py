c657b49af17502a9e9fef057bec3c4de5f4a19a8
shaylacarter <81391097+shaylacarter@users.noreply.github.com>
2021-04-23 23:55:01 -0400

    Update source.py
    Added matplotlib and fixed some typos/formatting errors

--- source.py
+++ source.py
@@ -2,6 +2,9 @@
 import csv
 import os
 import hashlib as hash
+import time
+import math
+import matplotlib.pyplot as plt
 
 print("Program Started")
 
@@ -13,39 +16,66 @@ MAX_PASS_LENGTH = 3
 wordlist = []
 wordlist.append("")
 file = open('dictionary.txt', 'r')
-filecontent=csv.reader(file)
+filecontent = csv.reader(file)
 for word in filecontent:
     wordlist.append(word[0])
 
 # take an input password and hash it
-plaintext_password = input("input your password: ")
-hasher = input("would you like to use (0) sha256 or (1) sha512")
+plaintext_password = input("Input your password: ")
+password_length = len(plaintext_password)
+hasher = input("Would you like to use (0) sha256 or (1) sha512?: ")
+hash_type = 'SHA256' if hasher else 'SHA512'
 hashed_password = hash.sha3_256() if hasher else hash.sha3_512()
 hashed_password.update(str.encode(plaintext_password))
+
+start = time.time()
 
 # go through every permutation of up to MAX_PASS_LENGTH words in wordlist
 counter = 1
 worked = False
+case_counter = 0
 while counter < pow(len(wordlist), MAX_PASS_LENGTH):
     current = ""
     temp = counter
     for i in range(MAX_PASS_LENGTH):
-        current += wordlist[temp%len(wordlist)]
+        current += wordlist[temp % len(wordlist)]
         temp = temp // len(wordlist)
 
     counter += 1
 
     # hash the permutation
-    curr_hashed = hash.sha3_256() if hasher else hash.sha3_512()
-    curr_hashed.update(str.encode(current))
-    # compare it to the hashed_password
-    if curr_hashed.hexdigest() == hashed_password.hexdigest():
-        # print the permuation if it was correct and exit the loop
-        print("The has " + curr_hashed.hexdigest() + " corresponds to " + current)
-        worked = True
-        break
+    
+    if len(current) == password_length:
+        case_counter += 1
+        # hash the permutation
+        curr_hashed = hash.sha3_256() if hasher else hash.sha3_512()
+        curr_hashed.update(str.encode(current))
+        # compare it to the hashed_password
+        if curr_hashed.hexdigest() == hashed_password.hexdigest():
+            # print the permuation if it was correct and exit the loop
+            print("The hash " + curr_hashed.hexdigest() + " corresponds to " + current)
+            worked = True
+            break
 
 # if up to MAX_PASS_LENGTH was not enough (or it was not a permutation of the words)
 # print out the password and a not found statement
 if not worked:
-    print("there was no combination that worked for " + plaintext_password)
+    print("There was no combination that worked for " + plaintext_password)
+
+elapsed_time = time.time() - start
+
+guesses = []
+time_passed = []
+
+for i in range (math.ceil(elapsed_time) * 100):
+    guesses.append((case_counter / elapsed_time) * i / 100)
+    time_passed.append(i / 100)
+    if (i / 100 > elapsed_time):
+        break
+
+plt.plot(time_passed, guesses)
+plt.xlabel('Time Elapsed (seconds)')
+plt.ylabel('Hashes Compared')
+plt.suptitle(str(case_counter) + ' crack attempts over ' + str(round(elapsed_time, 3)) + ' seconds', fontsize=14, fontweight='bold')
+plt.title('Dictionary size: ' + str(len(wordlist)) + ' words | Hash type: ' + hash_type)
+plt.show()
